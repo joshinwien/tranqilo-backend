1740f0c8a81df90ae531ef64e6dd19a9efbd9454
Joshua Pennypacker <yoshuapovell@gmail.com>
2025-06-07 18:27:21 +0200

    Initial commit

---
+++ src/main/java/com/tranqilo/TranqiloApplication.java
@@ -0,0 +1,13 @@
+package com.tranqilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranqiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranqiloApplication.class, args);
+	}
+
+}
